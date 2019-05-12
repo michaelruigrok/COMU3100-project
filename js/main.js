2f7eb4ef63f47ff6e98f09cbef4c34f73a9a09c5
Michael Ruigrok <ruigrok.michael@gmail.com>
2019-05-12 22:02:28 +1000

    update scroll to next section, make it customisable

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-function showHidden(query) {
+function showHidden(query, options) {
 	document.querySelectorAll(query).forEach(function(e) {
 		e.classList.add("fadeIn", "animated");
 		e.classList.remove("hidden");
-
-		// Scroll to next section
-		$([document.documentElement, document.body]).delay(300).animate({
-			scrollTop: $(e).offset().top - $(window).height() / 20
-		}, 1000);
 	});
 }
 
-function addQListener(qName) {
+function scrollToNext(jquery, options) {
+	console.log(options);
+	var options = options || {};
+	var delay = options.delay !== undefined ?
+		options.delay : 300;
+	var offset = options.offset !== undefined ?
+		options.offset : -($(window).height() / 22);
+	$([document.documentElement, document.body]).delay(delay).animate({
+		scrollTop: $(jquery).offset().top + offset
+	}, 1000);
+}
+
+function addQListener(qName, options) {
 	document.querySelectorAll("input[name='" + qName + "']")
 		.forEach(function(input) {
-			input.addEventListener("click",
-				() => showHidden("#" + qName + "_feedback"));
+			input.addEventListener("click", function(e) {
+				var feedback = "#" + qName + "_feedback";
+				showHidden(feedback);
+				scrollToNext(feedback, options || {});
+			});
 
 			if (input.checked) showHidden("#" + qName + "_feedback");
 		});
@@ -33,8 +43,8 @@ document.querySelectorAll("form.three-class p")
 
 
 addQListener("three-class");
-addQListener("commute-time");
 addQListener("employment-type");
+addQListener("commute-time", { offset: 0 });
 addQListener("books");
 
 
